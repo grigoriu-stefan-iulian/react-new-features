d613ffb3b9c2bc4409ebe8e5722831ed082427b8
grigoriu-stefan-iulian <grigoriu.stefan.iulian@gmail.com>
2019-04-01 15:38:50 +0300

    custom hooks

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,30 +1,14 @@
-import React, { useContext, useState, useEffect } from 'react'
+import React, { useContext } from 'react'
 import NotesContext from '../context/notes-context'
+import useMousePosition from '../hooks/useMousePosition'
 
-const useMousePosition = () => {
-    const [position, setPosition] = useState({ x: 0, y: 0 })
 
-    useEffect(() => {
-        const handleMouseMove = (e) => {
-            setPosition({
-                x: e.pageX,
-                y: e.pageY
-            })
-        }
-        document.addEventListener('mousemove', handleMouseMove)
-        return () => {
-            document.removeEventListener('mousemove', handleMouseMove)
-        }
-    }, [])
-    return position
-}
 
 const Note = ({ index, note }) => {
     const { dispatch } = useContext(NotesContext)
     const removeNote = (title) => dispatch({ type: 'REMOVE_NOTE', title })
-    const position = useMousePosition()
     // setNotes(notes.filter((note) => note.title !== title))
-
+    const position = useMousePosition()
     return (
         <div>
             <p>x: {position.x}, y: {position.y} </p>
